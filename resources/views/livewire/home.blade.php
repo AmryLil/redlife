19309a06d8505c73b87f83fd6fedb0ea39b44199
Ulil Amry <ulilamry432@gmail.com>
2025-02-19 03:04:23 +0700

    feat:add home page

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,4 +1,41 @@
-<div>
-    <h1>home</h1>
-    <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum dolore delectus mollitia fuga nisi quod enim saepe quo sunt, consectetur omnis, earum amet ipsam ratione eligendi. Libero eaque adipisci quos.</div>
-</div>
+    <div class="bg-red-500 w-full h-80 flex gap-2 text-slate-50 p-6 justify-between">
+        <div class="w-full space-y-5">
+            <div>
+                <h1 class="text-xl font-bold">Setetes Darah Kita, Nyawa Bagi Sesama</h1>
+                <h3 class="font-extralight">Bergabunglah dengan komunitas donor darah!</h3>
+            </div>
+
+            <div class="bg-white rounded-md p-3 h-max">
+                <div class="flex">
+                    <div>
+                        <x-label> Golongan Darah</x-label>
+                        <x-input></x-input>
+                    </div>
+
+                    <div>
+                        <x-label>Rhesus</x-label>
+                        <x-input></x-input>
+                    </div>
+                </div>
+                <x-button class="bg-red-600 w-full justify-center">Cari Sekarang</x-button>
+            </div>
+        </div>
+        <div class="grid grid-cols-2 gap-4 w-full">
+            <div>
+                <h1>150+</h1>
+                <h2>Pendonor Aktif</h2>
+            </div>
+            <div>
+                <h1>80</h1>
+                <h2>Tersedia</h2>
+            </div>
+            <div>
+                <h1>24</h1>
+                <h2>Jam Layanan</h2>
+            </div>
+            <div>
+                <h1>100%</h1>
+                <h2>Keamanan</h2>
+            </div>
+        </div>
+    </div>
